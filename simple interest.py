3dd246c9e3c19813f270cd1f6a916f3a86a113ff
xanterkushal <111531161+xanterkushal@users.noreply.github.com>
2023-05-08 14:00:08 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,10 @@
+# Simple interest in python
+p = int(input("Enter the amount: "))
+# Enter the number of years
+t = int(input("Enter the number of years: "))
+# Enter the rate of interest
+r = float(input("Enter the rate of interest: "))
+
+SimpleInterest = (p*r*t)/100
+
+print("The simple interset is:", SimpleInterest)
