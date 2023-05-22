7cb3bed972d9e8eb8c8b342e67c889719e0d417f
xanterkushal <111531161+xanterkushal@users.noreply.github.com>
2023-05-22 20:08:46 +0530

    Add files via upload

---
+++ volume of rectangle.py
@@ -0,0 +1,6 @@
+#Computing the volume of a rectangular box
+l=float(input("Enter the length"))
+b=float(input("Enter the breadth"))
+c=float(input("Enter the height"))
+v=l*b*h
+print("Volume of the rectangular box is :",v)
