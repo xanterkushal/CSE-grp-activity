73f1dbb2cd160be8ae6c7005e1336d0bdd9317d7
Komal-1308 <134276191+Komal-1308@users.noreply.github.com>
2023-05-22 20:34:02 +0530

    Update Dailywages.py

--- Dailywages.py
+++ Dailywages.py
@@ -1,6 +1,6 @@
 H = float(input("Enter the number of hours worked by a worker: "))
 R = float(input("Enter the rate of pay per hour of a worker: "))
 
-w = H*R
+W = H*R
 
 print("Daily wage of a worker: , W")
