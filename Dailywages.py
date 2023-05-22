aaa185c001e953ed7f702d253b29b4c33c01845f
Bhavana-34 <130453103+Bhavana-34@users.noreply.github.com>
2023-05-22 20:24:52 +0530

    Update Dailywages.py

--- Dailywages.py
+++ Dailywages.py
@@ -1,15 +1,4 @@
-def calculate_daily_wage(hours_worked, hourly_rate):
-    if hours_worked <= 8:
-        daily_wage = hours_worked * hourly_rate
-    else:
-        overtime_hours = hours_worked - 8
-        overtime_pay = overtime_hours * (hourly_rate * 1.5)
-        daily_wage = (8 * hourly_rate) + overtime_pay
-    return daily_wage
-
-# Example usage
-hours_worked = float(input("Enter the number of hours worked: "))
-hourly_rate = float(input("Enter the hourly rate: "))
-
-daily_wage = calculate_daily_wage(hours_worked, hourly_rate)
-print("Daily Wage: $", daily_wage)
+hours = 35.0
+rate= 12.50
+pay= hours*rate
+print(pay)
